43e57d40b43442cac2649098b75be8ff07fd1717
jgomezz <jgomezz@gmail.com>
2023-12-10 09:09:26 -0500

    Update

--- src/main/java/pe/edu/tecsup/springbootapp/controllers/FormController.java
+++ src/main/java/pe/edu/tecsup/springbootapp/controllers/FormController.java
@@ -1,7 +1,10 @@
 package pe.edu.tecsup.springbootapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -12,6 +15,16 @@ public class FormController {
 		return "form"; // WEB-INF/jsp/form.jsp
 
 		
+	}
+	
+
+	@PostMapping("/sendform")
+	public String procesarForm(Model model, @RequestParam("nombre") String nombre) {
+		
+		
+		System.out.println("nombre --> " +  nombre);
+		
+		return "resultadoForm"; // WEB-INF/jsp/resultadoForm.jsp
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/jsp/resultadoForm.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	
+ Resultado de formulario
+	
+</body>
+</html>
